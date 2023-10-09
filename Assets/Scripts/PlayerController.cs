04f3def288e6b68db7bf1774c188cde457d40676
TohaMeme <yuki.tempoin12@gmail.com>
2023-10-09 16:31:50 +0300

    09.10.2023 - movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,15 +18,50 @@ public class PlayerController : MonoBehaviour
 
     Vector3 surfaceNormal;
 
+    float verticalSpeed;
+
     private void Awake()
     {
         characterController= GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    float verticalSpeed;
-
     private void Update()
+    {
+        Vector3 rotation = new Vector3(0, Input.GetAxis("Mouse X") * _sensitivity * Time.deltaTime);
+        transform.Rotate(rotation);
+
+        if (characterController.isGrounded)
+            verticalSpeed = -0.1f;
+        else
+            verticalSpeed += Physics.gravity.y * Time.deltaTime;
+
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        input = Vector3.ClampMagnitude(input, 1);
+
+        Vector3 velocity = transform.TransformDirection(input) * _speed;
+
+        Quaternion slopeRotation = Quaternion.FromToRotation(Vector3.up, surfaceNormal);
+        Vector3 adjustedVelocity = slopeRotation * velocity;  
+
+        velocity = adjustedVelocity.y < 0 ? adjustedVelocity : velocity;
+
+        velocity.y += verticalSpeed;
+
+        characterController.Move(velocity * Time.deltaTime);
+
+    }
+
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Debug.DrawLine(hit.point, hit.point + hit.normal * 10, Color.red);
+        surfaceNormal = hit.normal;
+    }
+
+
+
+    /*private void Update()
     {
         float xMouseMovement = Input.GetAxis("Mouse X");
         float horizontal = Input.GetAxis("Horizontal");
@@ -35,7 +70,7 @@ public class PlayerController : MonoBehaviour
         Vector3 moveDirection = new Vector3(horizontal, 0, vertical);
 
         moveDirection = Vector3.ClampMagnitude(moveDirection, 1);
-        moveDirection = transform.TransformDirection(moveDirection) * _speed;
+        moveDirection = transform.TransformDirection(moveDirection);
         moveDirection = Vector3.ProjectOnPlane(moveDirection, surfaceNormal);
 
         Debug.DrawLine(transform.position, transform.position + moveDirection * 2, Color.blue);
@@ -49,16 +84,7 @@ public class PlayerController : MonoBehaviour
 
         characterController.Move((moveDirection * _speed + Vector3.up * verticalSpeed ) * Time.deltaTime);
         
-    }
-
-    private void OnControllerColliderHit(ControllerColliderHit hit)
-    {
-        Debug.DrawLine(hit.point, hit.point + hit.normal * 10, Color.red);
-        surfaceNormal = hit.normal;
-    }
-
-
-
+    }*/
 
     /*   void FixedUpdate()
        {
